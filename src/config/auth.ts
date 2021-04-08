014dce2364bc7697814d291abd1acb8c3b675893
sternlucas <contas.lucas@gmail.com>
2021-04-07 21:50:35 -0300

    First commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '6509b965cfd8fa756d04d85a0efc8a49',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execure({ name, email, password });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
+  return response.json({ ok: true });
+});
+
+export default usersRouter;
